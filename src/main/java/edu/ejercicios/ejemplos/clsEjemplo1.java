0b07ddc71d27bb12d49123e8550e764d35d504e0
Leonardo <crisleonardo100@gmail.com>
2023-07-22 10:55:36 -0600

    ejercicio en clase

---
+++ src/main/java/edu/ejercicios/ejemplos/clsEjemplo1.java
@@ -0,0 +1,21 @@
+package edu.ejercicios.ejemplos;
+
+public class clsEjemplo1 {
+
+
+
+
+    /*
+    //calculo (a+b)*(a-b)
+
+    public void calculo(){
+        int a,b;
+        int resultado;
+        a = 10;
+        b = 20;
+        resultado = (a+b)*(a-b);
+        System.out.println("El resultado es: "+resultado);
+    }
+    //System.out.println("Ingrese los datos que quiere operar");
+*/
+}
