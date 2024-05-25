4b0d5d5cab0519271721acdd514fb8b6044f3784
Leonardo Zepeda <jgamez@aseinfo.com.sv>
2024-05-25 08:05:53 -0600

    Primer commit Agenda

---
+++ Progra 3/Agenda/src/main/java/org/example/Modelo/Contacto.java
@@ -0,0 +1,53 @@
+package org.example.Modelo;
+
+import java.time.LocalDate;
+
+public class Contacto {
+    private String nombre;
+    private long telefono;
+    private String correoElectronico;
+    private LocalDate fechaNacimiento;
+
+    public Contacto(String nombre, long telefono, String correoElectronico, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correoElectronico = correoElectronico;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Contacto(String nombre, long telefono) {
+        this(nombre, telefono, "", null); // Llama al constructor principal
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
